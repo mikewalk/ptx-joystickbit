964392be3b8cefdd516b37e06d3a2120fe38bf74
Mike Walker <mikewalk@gmail.com>
2021-01-25 20:23:04 +0000

    first try to make an extension (crosses fingers)

--- main.ts
+++ main.ts
@@ -1,30 +1,61 @@
-input.onButtonPressed(Button.A, function () {
-    btnMode = false
-})
-input.onButtonPressed(Button.B, function () {
-    btnMode = true
-})
-let btnMode = false
-btnMode = false
-basic.forever(function () {
-    if (btnMode) {
-        if (joystickbit.buttonPressed(btnList.F, btnStateList.pressed)) {
-            basic.showString("F")
-        } else if (joystickbit.buttonPressed(btnList.E, btnStateList.pressed)) {
-            basic.showString("E")
-        } else if (joystickbit.buttonPressed(btnList.D, btnStateList.pressed)) {
-            basic.showString("D")
-        } else if (joystickbit.buttonPressed(btnList.C, btnStateList.pressed)) {
-            basic.showString("C")
-        } else if (joystickbit.buttonPressed(btnList.B, btnStateList.pressed)) {
-            basic.showString("B")
-        } else if (joystickbit.buttonPressed(btnList.A, btnStateList.pressed)) {
-            basic.showString("A")
-        } else {
-            basic.clearScreen()
-        }
-    } else {
-        basic.clearScreen()
-        led.plot(Math.map(joystickbit.JoystickX(), -512, 511, 0, 5), Math.map(joystickbit.JoystickY(), -512, 511, 0, 5))
+enum btnList {
+    //% block="none"
+    None=0,
+    //% block="A"
+    A=1,
+    //% block="B"
+    B=2,
+    //% block="C"
+    C=3,
+    //% block="D"
+    D=4,
+    //% block="E"
+    E=5,
+    //% block="F"
+    F=6
+}
+
+enum btnStateList {
+    //% block="Pressed"
+    pressed = 1,
+    //% block="Released"
+    released = 0
+}
+
+//% weight=0 color=#555555 icon="\uf11b"
+namespace joystickbit {
+
+    //% block="Joystick X position"
+    export function JoystickX(): number {
+        return Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, -512, 511)
     }
-})
+
+    //% block="Joystick Y position"
+    export function JoystickY(): number {
+        return Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -512, 511)
+    }
+
+    //% block="Button %btn|  %btnEvent"
+    //% btn.defl=btnList.A
+    export function buttonPressed(btn:btnList, btnState:btnStateList): boolean {
+        let btnRead = pins.analogReadPin(AnalogPin.P2)
+        let releaseCheck = (btnState == btnStateList.released)
+        
+        if (btnRead >= 0 &&  btnRead < 256 && btn == btnList.A) {
+            return (releaseCheck?false:true);
+        } else if (btnRead >= 256 && btnRead < 597 && btn == btnList.B) {
+            return (releaseCheck?false:true);
+        } else if (btnRead >= 597 && btnRead < 725 && btn == btnList.C) {
+            return (releaseCheck?false:true);
+        } else if (btnRead >= 725 && btnRead < 793 && btn == btnList.D) {
+            return (releaseCheck?false:true);
+        } else if (btnRead >= 793 && btnRead < 836 && btn == btnList.F) {
+            return (releaseCheck?false:true);
+        } else if (btnRead >= 836 && btnRead < 938 && btn == btnList.E) {
+            return (releaseCheck?false:true);
+        } else {
+            return (releaseCheck?true:false);
+        }
+    }
+
+}
